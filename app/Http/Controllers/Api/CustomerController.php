b91f9e223df2e925c561cb85eb040067a4270d5e
akasifislam <aseasifislam@gmail.com>
2021-04-18 23:23:02 +0600

    api create nd data updated

--- app/Http/Controllers/Api/CustomerController.php
+++ app/Http/Controllers/Api/CustomerController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Api;
 use App\Customer;
 use App\Http\Controllers\Controller;
 use App\Http\Resources\CustomerCollection;
+use App\Http\Resources\CustomerResource;
 use Illuminate\Http\Request;
 
 class CustomerController extends Controller
@@ -38,7 +39,7 @@ class CustomerController extends Controller
      */
     public function show($id)
     {
-        //
+        return new CustomerResource(Customer::findOrFail($id));
     }
 
     /**
